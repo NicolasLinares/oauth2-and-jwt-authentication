79fb1703c5f5c52b88f7152c731043a75da7f174
NicolasLinares <nlbarba97@gmail.com>
2022-04-14 18:29:37 +0200

    Set eslint with standard style

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true
+    },
+    extends: [
+        'standard'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {}
+}
