59f56c913c46c2f5dddb43466d5e59dac88cc1c6
NicolasLinares <nlbarba97@gmail.com>
2022-04-15 13:52:51 +0200

    response manager

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+const httpStatus = {
+    OK: 200,
+    NO_CONTENT: 204,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+}
+
+
+module.exports = httpStatus
---
+++ src/utils/responses.js
@@ -0,0 +1,32 @@
+const httpStatus = require("./constants")
+const logger = require("./log")
+
+const httpResponse = []
+httpResponse[httpStatus.OK] = (response, resource) => {
+    return response.json(resource)
+}
+httpResponse[httpStatus.NO_CONTENT] = (response) => {
+    return response.status(httpStatus.NO_CONTENT)
+}
+
+httpResponse[httpStatus.CREATED] = (response, resource) => {
+    let message = resource || `${httpStatus.CREATED} Created`
+    return response.status(httpStatus.CREATED).json({ data: message })
+}
+httpResponse[httpStatus.NOT_FOUND] = (response, msg = null) => {
+    let message = msg || `${httpStatus.NOT_FOUND} Not found`
+    logger.error(message)
+    return response.status(httpStatus.NOT_FOUND).json({ error: message })
+}
+httpResponse[httpStatus.BAD_REQUEST] = (response, msg = null) => {
+    let message = msg || `${httpStatus.BAD_REQUEST} Bad request`
+    logger.error(message)
+    return response.status(httpStatus.BAD_REQUEST).json({ error: message })
+}
+httpResponse[httpStatus.CONFLICT] = (response, msg = null) => {
+    let message = msg || `${httpStatus.CONFLICT} Conflict`
+    logger.error(message)
+    return response.status(httpStatus.CONFLICT).json({ error: message })
+}
+
+module.exports = httpResponse
