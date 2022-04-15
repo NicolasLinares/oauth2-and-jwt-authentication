131426934221af7c49b19948b3b65ff41488cdde
NicolasLinares <nlbarba97@gmail.com>
2022-04-15 13:53:41 +0200

    added delete, post, getters

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const logger = require("./utils/log/")
-const database = require("./database")
+const database = require("./database");
+const res = require("express/lib/response");
 
 logger.info("Deploying server...")
 
@@ -9,9 +10,16 @@ database.connect()
         logger.info("Database succesfully connected")
 
         const express = require("express")
-        const requestLogger = require("./middlewares/requestLogger")
-        const notFoundResponse = require("./middlewares/404")
+        var bodyParser = require('body-parser')
+
+        var requestLogger = require("./middlewares/requestLogger")
+        var httpResponse = require("./utils/responses")
+        var httpStatus = require("./utils/constants")
+
         const app = express()
+
+        var jsonParser = bodyParser.json()
+
 
         // Middleware 
         app.use(requestLogger);
@@ -20,21 +28,66 @@ database.connect()
             response.send('<h1> Hello world! </h1>')
         })
 
-        app.get('/users', (request, response) => {
-            database.getUsers()
-                .then(result => {
-                    response.contentType = "application/json"
-                    response.send(result)
+        app.get('/api/users/:username', (request, response) => {
+            const username = request.params.username
+
+            database.getUser(username)
+                .then(user => {
+                    httpResponse[httpStatus.OK](response, user)
                 })
                 .catch(err => {
-                    logger.error(err)
+                    httpResponse[httpStatus.NOT_FOUND](response, err)
+                })
+        })
+
+        app.delete('/api/users/:username', (request, response) => {
+            const username = request.params.username
+
+            database.deleteUser(username)
+                .then(() => {
+                    httpResponse[httpStatus.NO_CONTENT](response)
+                })
+                .catch(err => {
+                    httpResponse[httpStatus.CONFLICT](response, err)
+                })
+        })
+
+        app.get('/api/users', (request, response) => {
+            database.getUsers()
+                .then(users => {
+                    response.json(users)
+                })
+                .catch(err => {
+                    httpResponse[httpStatus.NOT_FOUND](response, err)
+                })
+        })
+
+        app.post('/api/users', jsonParser, (request, response) => {
+            const user = request.body
+
+            if (!user.name) {
+                return httpResponse[httpStatus.BAD_REQUEST](response, "required 'name' field is missing")
+            }
+            if (!user.username) {
+                return httpResponse[httpStatus.BAD_REQUEST](response, "required 'username' field is missing")
+            }
+            if (!user.password) {
+                return httpResponse[httpStatus.BAD_REQUEST](response, "required 'password' field is missing")
+            }
+
+            database.addUser(user)
+                .then((savedUser) => {
+                    httpResponse[httpStatus.CREATED](response, savedUser)
+                })
+                .catch(err => {
+                    httpResponse[httpStatus.CONFLICT](response, err.message)
                 })
         })
 
         // Middleware 
-        app.use(notFoundResponse)
+        app.use((request, response) => httpResponse[httpStatus.NOT_FOUND](response))
 
-        const PORT = process.env.SERVER_PORT
+        const PORT = process.env.SERVER_PORT || 3001
         app.listen(PORT, () => {
             logger.info(`Server running on port ${PORT}`)
         })
