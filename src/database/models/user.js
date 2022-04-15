a40a713afd9108c7b6ccee86a5161fab2a1d1d7c
NicolasLinares <nlbarba97@gmail.com>
2022-04-15 11:37:58 +0200

    Exclude password by default and delete _id and __v when retrieving the user model

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -3,7 +3,15 @@ const { Schema, model } = require("mongoose")
 const userSchema = new Schema({
     name: String,
     username: String,
-    password: String,
+    password: { type: String, select: false }
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 const User = model('User', userSchema)
