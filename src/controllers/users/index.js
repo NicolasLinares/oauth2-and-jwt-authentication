fa67b96cfc357766362b3ae85ecc5e194f2105d8
NicolasLinares <nlbarba97@gmail.com>
2022-04-19 15:50:22 +0200

    create user controller for all user requests

---
+++ src/controllers/users/index.js
@@ -0,0 +1,77 @@
+const database = require("../../database");
+var httpResponse = require("../../utils/responses")
+var httpStatus = require("../../utils/constants")
+
+function UserController() {
+
+    this.getUserById = (request, response) => {
+        const id = request.params.id
+        database.getUser(id)
+            .then(user => {
+                httpResponse[httpStatus.OK](response, user)
+            })
+            .catch(err => {
+                httpResponse[httpStatus.NOT_FOUND](response, err)
+            })
+    }
+
+    this.updateUserById = (request, response) => {
+        const id = request.params.id
+        const update = request.body
+        database.updateUser(id, update)
+            .then((updatedUser) => {
+                httpResponse[httpStatus.OK](response, updatedUser)
+            })
+            .catch(err => {
+                httpResponse[httpStatus.CONFLICT](response, err)
+            })
+    }
+
+    this.deleteUserById = (request, response) => {
+        const id = request.params.id
+        database.deleteUser(id)
+            .then(() => {
+                httpResponse[httpStatus.NO_CONTENT](response)
+            })
+            .catch(err => {
+                httpResponse[httpStatus.CONFLICT](response, err)
+            })
+    }
+
+    this.getAllUsers = (request, response) => {
+        database.getUsers()
+            .then(users => {
+                response.json(users)
+            })
+            .catch(err => {
+                httpResponse[httpStatus.NOT_FOUND](response, err)
+            })
+    }
+
+    this.createUser = (request, response) => {
+        const user = request.body
+
+        if (!user.name) {
+            return httpResponse[httpStatus.BAD_REQUEST](response, "required 'name' field is missing")
+        }
+        if (!user.username) {
+            return httpResponse[httpStatus.BAD_REQUEST](response, "required 'username' field is missing")
+        }
+        if (!user.password) {
+            return httpResponse[httpStatus.BAD_REQUEST](response, "required 'password' field is missing")
+        }
+
+        database.addUser(user)
+            .then((savedUser) => {
+                httpResponse[httpStatus.CREATED](response, savedUser)
+            })
+            .catch(err => {
+                httpResponse[httpStatus.CONFLICT](response, err.message)
+            })
+    }
+
+}
+
+const userController = new UserController()
+
+module.exports = userController
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 require("dotenv").config();
 const logger = require("./utils/log/")
 const database = require("./database");
+const express = require("express")
+var bodyParser = require("body-parser")
+var jsonParser = bodyParser.json()
+var requestLogger = require("./middlewares/requestLogger")
+var httpResponse = require("./utils/responses")
+var httpStatus = require("./utils/constants")
+var path = require("path")
+
+const userController = require("./controllers/users")
 
 logger.info("Deploying server...")
 
@@ -8,90 +17,18 @@ database.connect()
     .then(() => {
         logger.info("Database succesfully connected")
 
-        const express = require("express")
-        var bodyParser = require('body-parser')
-
-        var requestLogger = require("./middlewares/requestLogger")
-        var httpResponse = require("./utils/responses")
-        var httpStatus = require("./utils/constants")
-
         const app = express()
-
-        var jsonParser = bodyParser.json()
-
 
         // Middleware 
         app.use(requestLogger);
 
-        app.get('/', (request, response) => {
-            response.send('<h1> Hello world! </h1>')
-        })
+        app.use(express.static(path.join(__dirname, '../public')))
 
-        app.get('/api/users/:id', (request, response) => {
-            const id = request.params.id
-            database.getUser(id)
-                .then(user => {
-                    httpResponse[httpStatus.OK](response, user)
-                })
-                .catch(err => {
-                    httpResponse[httpStatus.NOT_FOUND](response, err)
-                })
-        })
-
-        app.put('/api/users/:id', jsonParser, (request, response) => {
-            const id = request.params.id
-            const update = request.body
-            database.updateUser(id, update)
-                .then((updatedUser) => {
-                    httpResponse[httpStatus.OK](response, updatedUser)
-                })
-                .catch(err => {
-                    httpResponse[httpStatus.CONFLICT](response, err)
-                })
-        })
-
-        app.delete('/api/users/:id', (request, response) => {
-            const id = request.params.id
-            database.deleteUser(id)
-                .then(() => {
-                    httpResponse[httpStatus.NO_CONTENT](response)
-                })
-                .catch(err => {
-                    httpResponse[httpStatus.CONFLICT](response, err)
-                })
-        })
-
-        app.get('/api/users', (request, response) => {
-            database.getUsers()
-                .then(users => {
-                    response.json(users)
-                })
-                .catch(err => {
-                    httpResponse[httpStatus.NOT_FOUND](response, err)
-                })
-        })
-
-        app.post('/api/users', jsonParser, (request, response) => {
-            const user = request.body
-
-            if (!user.name) {
-                return httpResponse[httpStatus.BAD_REQUEST](response, "required 'name' field is missing")
-            }
-            if (!user.username) {
-                return httpResponse[httpStatus.BAD_REQUEST](response, "required 'username' field is missing")
-            }
-            if (!user.password) {
-                return httpResponse[httpStatus.BAD_REQUEST](response, "required 'password' field is missing")
-            }
-
-            database.addUser(user)
-                .then((savedUser) => {
-                    httpResponse[httpStatus.CREATED](response, savedUser)
-                })
-                .catch(err => {
-                    httpResponse[httpStatus.CONFLICT](response, err.message)
-                })
-        })
+        app.post('/api/users', jsonParser, userController.createUser)
+        app.get('/api/users', userController.getAllUsers)
+        app.get('/api/users/:id', userController.getUserById)
+        app.put('/api/users/:id', jsonParser, userController.updateUserById)
+        app.delete('/api/users/:id', userController.deleteUserById)
 
         // Middleware 
         app.use((request, response) => httpResponse[httpStatus.NOT_FOUND](response))
